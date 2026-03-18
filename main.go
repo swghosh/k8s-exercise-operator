d3b785d90d6a52f5c2739ffd6bdc44cec612549c
Swarup Ghosh <swghosh@redhat.com>
2026-03-18 20:10:10 +0530

    add(leaderelection): implement leader election adapted from https://sklar.rocks/kubernetes-leader-election/

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -11,10 +12,9 @@ import (
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/leaderelection"
+	rl "k8s.io/client-go/tools/leaderelection/resourcelock"
 	"k8s.io/klog/v2"
-
-	// Uncomment the following line to load the gcp plugin (only required to authenticate against GKE clusters).
-	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 
 	clientset "github.com/swghosh/k8s-exercise-operator/pkg/generated/clientset/versioned"
 	informers "github.com/swghosh/k8s-exercise-operator/pkg/generated/informers/externalversions"
@@ -26,6 +26,14 @@ var (
 )
 
 const resyncPeriod = time.Minute * 2
+
+var (
+	lockName      = "k8s-exercise-operator-lock"
+	lockNamespace = "default"
+
+	// identity is "<PID>-<HOSTNAME>", env var $HOSTNAME provides name of the pod.
+	identity = fmt.Sprintf("%d-%s", os.Getpid(), os.Getenv("HOSTNAME"))
+)
 
 var onlyOneSignalHandler = make(chan struct{})
 var shutdownSignals = []os.Signal{os.Interrupt, syscall.SIGTERM}
@@ -56,22 +64,60 @@ func main() {
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
 	}
 
-	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, resyncPeriod)
-	// exampleInformerFactory := informers.NewSharedInformerFactory(exampleClient, resyncPeriod)
-	myInformerFactory := informers.NewSharedInformerFactory(myClient, resyncPeriod)
-
-	controller := NewController(ctx, kubeClient, myClient,
-		kubeInformerFactory.Apps().V1().Deployments(),
-		myInformerFactory.Cache().V1alpha1().Memcacheds())
-
-	// notice that there is no need to run Start methods in a separate goroutine. (i.e. go kubeInformerFactory.Start(ctx.done())
-	// Start method is non-blocking and runs all registered informers in a dedicated goroutine.
-	kubeInformerFactory.Start(ctx.Done())
-	myInformerFactory.Start(ctx.Done())
-
-	if err = controller.Run(ctx, 2); err != nil {
-		logger.Error(err, "Error running controller")
+	// Create a lease-based lock for leader election
+	lock, err := rl.NewFromKubeconfig(
+		rl.LeasesResourceLock,
+		lockNamespace,
+		lockName,
+		rl.ResourceLockConfig{
+			Identity: identity,
+		},
+		cfg,
+		time.Second*10,
+	)
+	if err != nil {
+		logger.Error(err, "Error creating resource lock")
 		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+	}
+
+	// Start the leader election loop; only the leader runs the controller
+	leaderelection.RunOrDie(ctx, leaderelection.LeaderElectionConfig{
+		LeaseDuration: time.Second * 15,
+		RenewDeadline: time.Second * 10,
+		RetryPeriod:   time.Second * 2,
+
+		Lock: lock,
+		Name: lockName,
+
+		Callbacks: leaderelection.LeaderCallbacks{
+			OnStartedLeading: controllerStartupFunc(kubeClient, myClient, logger),
+			OnStoppedLeading: func() {
+				logger.Info("Leader election lost")
+				klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+			},
+			OnNewLeader: func(id string) {
+				logger.Info("New leader elected", "identity", id)
+			},
+		},
+	})
+}
+
+func controllerStartupFunc(kubeClient kubernetes.Interface, myClient clientset.Interface, logger klog.Logger) func(context.Context) {
+	return func(ctx context.Context) {
+		kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, resyncPeriod)
+		myInformerFactory := informers.NewSharedInformerFactory(myClient, resyncPeriod)
+
+		controller := NewController(ctx, kubeClient, myClient,
+			kubeInformerFactory.Apps().V1().Deployments(),
+			myInformerFactory.Cache().V1alpha1().Memcacheds())
+
+		kubeInformerFactory.Start(ctx.Done())
+		myInformerFactory.Start(ctx.Done())
+
+		if err := controller.Run(ctx, 2); err != nil {
+			logger.Error(err, "Error running controller")
+			klog.FlushAndExit(klog.ExitFlushTimeout, 1)
+		}
 	}
 }
 
